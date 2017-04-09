d590542dc45bfb9b98cec99757f4dca4c716517f
lor104 <lauren.rosentzveig@gmail.com>
2017-04-09 13:10:27 -0400

    run a couple tests on shopping-cart.rb

--- shopping-cart.rb
+++ shopping-cart.rb
@@ -8,8 +8,10 @@ class ShoppingCart
     @cart = []
   end
 
-  def add(name, base_price, tax_rate)
-    @cart << Stuff.new(name, base_price, tax_rate)
+  def add(name, base_price, tax_rate, quantity)
+    quantity.times do
+      @cart << Stuff.new(name, base_price, tax_rate)
+    end
     return @cart
   end
 
@@ -37,6 +39,32 @@ class ShoppingCart
 end
 
 
+# from stuff.rb
 # def initialize(name, base_price, tax_rate)
-#   @product = {:name => name, :base_price = base_price, :tax_rate => tax_rate}
+#   @name = name
+#   @base_price = base_price
+#   @tax_rate = tax_rate
 # end
+
+#tests:
+c = ShoppingCart.new
+c.add("banana", 4, 0.13, 2)
+c.add("tomato", 3, 0.15, 6)
+c.add("celery", 5, 0.7, 1)
+c.add("mushroom", 8, 0.13, 2)
+puts c.cart.inspect
+puts "Total cost of your cart without tax is: #{c.total_cost}"
+#should return -- $47
+puts "Total cost of your cart with tax is: #{c.total_cost_with_tax}"
+#should return -- 9.04 + 20.7 + 5.35 + 18.08 = $53.169999999999995
+
+a = ShoppingCart.new
+a.add("jeans", 56, 0.13, 1)
+a.add("tshirt", 18, 0.15, 2)
+a.add("coat", 115, 0.15, 1)
+a.add("socks", 7, 0.13, 6)
+puts a.cart.inspect
+puts "Total cost of your cart without tax is: #{a.total_cost}"
+#should return -- $249
+puts "Total cost of your cart with tax is: #{a.total_cost_with_tax}"
+#should return -- 63.28 + 41.4 + 132.25 + 47.46 = $284.39
