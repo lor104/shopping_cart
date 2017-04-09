863dc59a524212c534f36582879f2b12435282d4
lor104 <lauren.rosentzveig@gmail.com>
2017-04-09 12:54:40 -0400

    define add, remove, total_cost and total_cost_with_tax

--- shopping-cart.rb
+++ shopping-cart.rb
@@ -1,4 +1,42 @@
 require './stuff.rb'
 
 class ShoppingCart
+
+  attr_reader :cart
+
+  def initialize
+    @cart = []
+  end
+
+  def add(name, base_price, tax_rate)
+    @cart << Stuff.new(name, base_price, tax_rate)
+    return @cart
+  end
+
+  def remove(name)
+    @cart.delete_if {name == @cart.name}
+  end
+
+  def total_cost
+    total = 0
+    @cart.each do |product|
+      total += product.base_price
+    end
+    return total
+  end
+
+  def total_cost_with_tax
+    total = 0
+    @cart.each do |product|
+      price = product.base_price * (1 + product.tax_rate)
+      total += price
+    end
+    return total
+  end
+
 end
+
+
+# def initialize(name, base_price, tax_rate)
+#   @product = {:name => name, :base_price = base_price, :tax_rate => tax_rate}
+# end
--- stuff.rb
+++ stuff.rb
@@ -1,2 +1,11 @@
 class Stuff
+
+  attr_reader :name, :base_price, :tax_rate
+
+  def initialize(name, base_price, tax_rate)
+    @name = name
+    @base_price = base_price
+    @tax_rate = tax_rate
+  end
+
 end
